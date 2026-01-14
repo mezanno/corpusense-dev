bd4da182dc82e50f0d50b9af06b613b49278b2b8
Jonathan <jonathan.perrinet@epita.fr>
2026-01-14 14:04:47 +0100

    ✨ add batch processing options to WorkersMenu and update dialog handling for worker scope validation

--- src/components/menu/WorkersMenu.tsx
+++ src/components/menu/WorkersMenu.tsx
@@ -1,48 +1,65 @@
-import { Scope } from '@/data/models/Scope';
+import { isCollectionScope, Scope } from '@/data/models/Scope';
 import { useAppDispatch, useAppSelector } from '@/hooks/hooks';
 import { startWorkerProcessRequest } from '@/state/reducers/workers';
 import { selectWorkerPluginsInfo } from '@/state/selectors/workers';
 import { PocketKnife, ScanText } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+import { useAlertDialogContext } from '../reducers/useAlertDialogContext';
 import MultiOptionsMenu from './MultiOptionsMenu';
 
-const WorkersMenu = ({
-  scope,
-  // getActions,
-}: {
-  scope: Scope;
-  // getActions?: (pluginName: string) => (() => void) | undefined;
-}) => {
+const WorkersMenu = ({ scope }: { scope: Scope }) => {
+  const { t } = useTranslation();
   const appDispatch = useAppDispatch();
   const pluginsInfo = useAppSelector(selectWorkerPluginsInfo);
+  const { openDialog } = useAlertDialogContext();
 
   const params = {
     name: 'btn_start_analysis',
     icon: <PocketKnife />,
     info: 'info_start_analysis',
     items: pluginsInfo.map((plugin) => {
-      // const action = getActions
-      //   ? getActions(plugin.name)
-      //   : () => {
-      //       appDispatch(
-      //         startWorkerProcess({
-      //           workerName: plugin.name,
-      //           params: {},
-      //           scope,
-      //         }),
-      //       );
-      //     };
       return {
         name: plugin.displayName ?? plugin.name,
         description: plugin.description,
         icon: <ScanText />,
         action: () => {
-          appDispatch(
-            startWorkerProcessRequest({
-              workerName: plugin.name,
-              params: {},
-              scope,
-            }),
-          );
+          if (plugin.batchCompatible === true && isCollectionScope(scope)) {
+            openDialog({
+              title: t('title_worker_scope_validation'),
+              description: t('description_worker_scope_all'),
+              onConfirm: {
+                message: t('btn_worker_batch_yes'),
+                action: () =>
+                  appDispatch(
+                    startWorkerProcessRequest({
+                      workerName: plugin.name,
+                      params: {},
+                      scope,
+                      batchMode: true,
+                    }),
+                  ),
+              },
+              onCancel: {
+                message: t('btn_worker_batch_no'),
+                action: () =>
+                  appDispatch(
+                    startWorkerProcessRequest({
+                      workerName: plugin.name,
+                      params: {},
+                      scope,
+                    }),
+                  ),
+              },
+            });
+          } else {
+            appDispatch(
+              startWorkerProcessRequest({
+                workerName: plugin.name,
+                params: {},
+                scope,
+              }),
+            );
+          }
         },
         category: plugin.category,
       };
