2a246791f1780dea774d32e35d87932530ac884e
Jonathan <jonathan.perrinet@epita.fr>
2026-01-27 10:57:37 +0100

    🗃️ update supabase configuration

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from './supabase';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export type Job = Database['public']['Tables']['cs_jobs']['Row'];
+export type UserFile = Database['public']['Views']['user_files']['Row'];
