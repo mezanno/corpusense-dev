80cf499a56d84d8af3a9d7b1d18860ecb55b925a
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-04-08 14:07:48 +0200

    ✨ enhance IIIFSourcesDashboard: add dropdown menu for removing unused sources and improve layout

--- src/components/storage/IIIFSourcesDashboard.tsx
+++ src/components/storage/IIIFSourcesDashboard.tsx
@@ -1,28 +1,52 @@
 import useLiveSources from '@/hooks/data/sources/useLiveSources';
 import useDialog from '@/hooks/ui/useDialog';
-import { Globe, Plus } from 'lucide-react';
+import { Ellipsis, Globe, Plus } from 'lucide-react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card, CardContent } from '../ui/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
 import { ManifestCard } from './ManifestCard';
 
 const IIIFSourcesDashboard = () => {
   const { t } = useTranslation();
   const { openOpenManifestDialog } = useDialog();
-  const { remoteSources } = useLiveSources();
+  const { remoteSources, removeUnusedSources } = useLiveSources();
   const [lastManifestAddedId, setLastManifestAddedId] = useState<string | null>(null);
 
   const onManifestAdded = (newSourceId: string) => {
     setLastManifestAddedId(newSourceId);
   };
 
+  const handleRemoveUnusedSources = async () => {
+    await removeUnusedSources();
+  };
+
   return (
     <div className='flex h-full flex-col'>
-      <h1 className='flex items-center text-2xl font-bold'>
-        <Globe className='mr-2' /> {t('page_title_iiif_storage')}
+      <h1 className='flex items-center gap-2 text-2xl font-bold'>
+        <Globe />
+        <span>{t('page_title_iiif_storage')}</span>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Ellipsis className='hover:text-secondary' />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuGroup>
+              <DropdownMenuItem onClick={void handleRemoveUnusedSources()}>
+                {t('btn_remove_unused_sources')}
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </h1>
 
-      <div className='flex h-full flex-wrap gap-2 overflow-y-auto'>
+      <div className='mt-2 flex h-full flex-wrap gap-2 overflow-y-auto'>
         {/* max-h-[70vh] */}
         <Card
           className='card-file border-dashed'
