564a0ed2a0075fd6257616cc52b2d854d12345bf
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-07-07 15:28:09 +0200

    🦺  add model structure validation to import functionality

--- src/hooks/data/models/useModelIO.tsx
+++ src/hooks/data/models/useModelIO.tsx
@@ -1,4 +1,4 @@
-import { DataModel } from '@/data/models/DataModel';
+import { DataModel, DataModelSchema } from '@/data/models/DataModel';
 import { getModelRepository } from '@/data/repositories/indexeddb/dbFactory';
 import { useAppDispatch } from '@/hooks/hooks';
 import i18n from '@/i18n';
@@ -37,7 +37,11 @@ export const useModelIO = () => {
 
   const importModel = async (data: object, overwrite: boolean) => {
     try {
-      const model = data as DataModel; //TODO: validate the model structure and display error if invalid
+      const validation = DataModelSchema.safeParse(data);
+      if (!validation.success) {
+        throw new Error('Invalid model structure');
+      }
+      const model = validation.data;
       console.log(model);
 
       const existingModel = await modelRepository.getByName(model.name);
