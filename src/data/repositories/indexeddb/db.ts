88c80ec91f1bf0f95568c836483042f9fa28f5f0
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-08-12 11:40:01 +0200

    chore: update IndexedDB version from 18 to 30

--- src/data/repositories/indexeddb/db.ts
+++ src/data/repositories/indexeddb/db.ts
@@ -42,7 +42,7 @@ const db = new Dexie('mezanno') as Dexie & {
   storedBlobs: EntityTable<StoredBlob, 'id'>;
 };
 
-db.version(18)
+db.version(30)
   .stores({
     collections: '&id, name, *tags.id',
     collectionContents: '&id',
