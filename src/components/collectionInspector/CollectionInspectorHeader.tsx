ec60699b2fb7e05d9b0ef6e6914da58cfe01df03
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-09-04 17:52:51 +0200

    feat: :children_crossing: change the title of the accordion depending on its state

--- src/components/collectionInspector/CollectionInspectorHeader.tsx
+++ src/components/collectionInspector/CollectionInspectorHeader.tsx
@@ -7,10 +7,12 @@ import {
 } from '@/components/ui/accordion';
 import { Collection } from '@/data/models/collection';
 import 'gridstack/dist/gridstack.min.css';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const CollectionInspectorHeader = (collection: Collection) => {
   const { t } = useTranslation();
+  const [openPanel, setOpenPanel] = useState<string | undefined>(undefined);
 
   return (
     <Accordion
@@ -18,12 +20,21 @@ const CollectionInspectorHeader = (collection: Collection) => {
       className='panel flex-col'
       type='single'
       collapsible
+      value={openPanel}
+      onValueChange={setOpenPanel}
       // defaultValue='metadata' //this open the metadata by default
     >
       <AccordionItem value='metadata'>
         <AccordionTrigger className='mx-2'>
           <div>
-            <h2 className='flex items-center gap-2 text-lg'>
+            <h2
+              className='flex items-center gap-2 text-lg'
+              title={
+                openPanel === 'metadata'
+                  ? t('info_close_collection_metadata_panel')
+                  : t('info_open_collection_metadata_panel')
+              }
+            >
               {t('title_metadata_collection')}
               <span className='font-bold italic'>{collection.name}</span>
               <span>- {t('info_number_of_items', { number: collection.contentSize })}</span>
